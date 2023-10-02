bf6dbcb5e51529068766840779e38337e08846f1
Ilya Makarevich <>
2023-10-02 10:23:56 +0300

    add project files

---
+++ SimpleCurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCurrencyConverter
+//
+//  Created by Ilya Makarevich on 2.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
