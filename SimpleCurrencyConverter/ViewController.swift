46d470c9ba400744fb43650633fdd8a78afcc167
Ilya Makarevich <>
2023-10-02 10:43:07 +0300

    make UI in storyboard

--- SimpleCurrencyConverter/ViewController.swift
+++ SimpleCurrencyConverter/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private var pickerFrom: UIPickerView!
+    @IBOutlet private var pickerTo: UIPickerView!
+    @IBOutlet private var amountTextField: UITextField!
+    @IBOutlet private var convertButton: UIButton!
+    @IBOutlet private var resultLabel: UILabel!
+    @IBOutlet private var addToFavoritesButton: UIButton!
+    @IBOutlet private var removeFromFavoritesButton: UIButton!
+    @IBOutlet private var showAllButton: UIButton!
+    @IBOutlet private var showFavoritesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction private func convertButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction private func addToFavoritesAction(_ sender: Any) {
+    }
+    
+    @IBAction private func removeFromFavorites(_ sender: Any) {
+    }
+    
+    @IBAction private func showAllAction(_ sender: Any) {
+    }
+    
+    @IBAction private func showFavoritesAction(_ sender: Any) {
+    }
+    
 }
 
